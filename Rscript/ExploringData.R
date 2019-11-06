7853206df3749aa821b1c4c67aa6cf951d352023
Jesse Borden <jborden@ufl.edu>
2019-11-06 14:06:56 -0500

    Clean script and reorganize code to make it more easily readable by multiple contributors

--- Rscript/ExploringData.R
+++ Rscript/ExploringData.R
@@ -1,10 +1,31 @@
-
-
+#
+#This script is to start to clean and explore the data 
+#
 
 # data
 load(file="./output/ALLDATA.RData")
 head(ALLDATA,10)
 str(ALLDATA)
+
+# libraries
 library(tidyverse)
-summary_table<-ALLDATA %>% filter(PY==2019) %>% group_by(SO,PY) %>% summarize(n=n_distinct(DI))
-summary_table2<-ALLDATA %>% filter(PY==2019) %>% group_by(jrnl_type,SO) %>% summarize(n=n_distinct(DI))
+library(dplyr)
+library(ggplot2)
+
+head(ALLDATA)
+AllData <- ALLDATA %>%
+  select(DOI = DI, Journal = SO, Year = PY, AuthorNum = author,
+         Country = country, CountryCode = country_code, JrnlType = jrnl_type)
+
+# group 
+summary_table <- AllData %>% 
+  filter(Year==2019) %>% 
+  group_by(Journal,Year) %>% 
+  summarize(n=n_distinct(DOI))
+
+summary_table2<-AllData %>% 
+  filter(Year==2019) %>% 
+  group_by(JrnlType, Journal) %>% 
+  summarize(n=n_distinct(DOI))
+
+
