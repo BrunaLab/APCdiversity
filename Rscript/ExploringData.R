d09d58f07287153a4037f75257592ba4aed49bef
Jesse Borden <jborden@ufl.edu>
2019-11-06 14:27:31 -0500

    Subets by first and last author, generate OVERALL richness WITHOUT controlling for sample size

--- Rscript/ExploringData.R
+++ Rscript/ExploringData.R
@@ -17,6 +17,7 @@ AllData <- ALLDATA %>%
   select(DOI = DI, Journal = SO, Year = PY, AuthorNum = author,
          Country = country, CountryCode = country_code, JrnlType = jrnl_type)
 
+
 # group 
 summary_table <- AllData %>% 
   filter(Year==2019) %>% 
@@ -29,4 +30,29 @@ summary_table2<-AllData %>%
   summarize(n=n_distinct(DOI))
  
 
+#Data subsets
+
+# first author
+FirstAuthors <- AllData %>%
+  filter(AuthorNum ==1)
+#last author
+LastAuthors <- AllData %>%
+  group_by(DOI) %>%
+  arrange(AuthorNum) %>%
+  slice(n()) %>%
+  ungroup
+
+
+##
+#CountryRichness
+##
+# total country richness between the two journal types for FIRST authors
+# this does not control for sample size
+TotalRichFirst <- FirstAuthors %>% 
+  group_by(JrnlType) %>%
+  summarise(Rich = n_distinct(Country))
+# same as above for last authors
+TotalRichLast <- LastAuthors %>% 
+  group_by(JrnlType) %>%
+  summarise(Rich = n_distinct(Country))
 
