fb1f796a17141150db0117272ed50a9afad7fa89
Jesse Borden <jborden@ufl.edu>
2019-11-07 10:49:02 -0500

    Add some notes

--- Rscript/ExploringData.R
+++ Rscript/ExploringData.R
@@ -90,10 +90,11 @@ LastAuthPW <- LastAuth %>%
 ##################################################
 #SUBSET Paywall Journals by the number found in Open Access Journals
 ##################################################
-SamplePW <- FirstAuthPW %>%
+SamplePW <- FirstAuthPW %>% #subset the paywall journals First Author Data 
   filter(DOI != "NA") %>%
   group_by(Journal)%>%
-  sample_n(30)
+  sample_n(30) # this code only grabs 30 from each journal, which is a random number SIMILAR to the 
+#numbers of articles in the open access journals
 
 ##
 #CountryRichness
