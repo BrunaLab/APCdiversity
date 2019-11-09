4074072d2f8aef8f1a0cabc14fb55746933e3fc1
Jesse Borden <jborden@ufl.edu>
2019-11-08 20:17:51 -0500

    adding in Chris's code to randomly sample

--- Rscript/ExploringData.R
+++ Rscript/ExploringData.R
@@ -10,6 +10,7 @@
 library(tidyverse)
 library(dplyr)
 library(ggplot2)
+library(sampling)
 
 # data
 load(file="./output/ALLDATA.RData")
@@ -97,10 +98,30 @@ LastAuthPW <- LastAuth %>%
 #SUBSET Paywall Journals by the number found in Open Access Journals
 ##################################################
 #
-SamplePW <- FirstAuthPW %>% #subset the paywall journals First Author Data 
+#this is a way to randomly sample from each Paywall journal to 
+#match the number of articles in its OA mirror journal --CKG
+
+FirstAuthPWAlphab<-FirstAuthPW[order(FirstAuthPW$Journal),]#sort the paywall article df alphabetically by journal title
+SamplePW1 <- strata(FirstAuthPWAlphab, "Journal", 
+            size = c(14,6,47,9,29,21,9,5,10,10,9,31,16,32,6,8,6,30,28,32,9,17,32,2,8,1,14,33,14,18,36),
+            method = "srswor")
+
+
+sum(NumbArtOA$n) #using this to double check the sample size
+# the code above!
+# for each strata of journal, I took the number of articles available in the OA mirror 
+# journals, and randomly sampled articles in PW journals based on those numbers. 
+# I got the number of articles per OA journal from the NumbArtOA dataframe and just typed theminto the 'size' argument.
+# Two issues: 1.) the sum of articles in NumbArtOA is 542, but the length of the FirstAuthOA dataframe is 553
+# There are 11 articles in NumbArtOA that are 2018 articles, not 2019. I don't know why they weren't excluded in lines 47 and 52.
+# specifically, they are all water research articles in theFirstAuthOA df. (lines 37, 57, 81, 116, 242, 293, 383, 392, 497, 510, 515)
+# 2.) sleep medicine and research policy have 1 and 2 articles in those journals, respectively, according to NumbArtOA. I feel like we should delete them, 
+# as we probably can't get a good diversity estimate from such a small number of articles.
+
+SamplePW2 <- FirstAuthPW %>% #subset the paywall journals First Author Data 
   filter(DOI != "NA") %>%
   group_by(Journal)%>%
-  sample_n(30) # this code only grabs 30 from each journal, which is a random number SIMILAR to the 
+  sample_n(c(14,6,47,9,29,21,9,5,10,10,9,31,16,32,6,8,6,30,28,32,9,17,32,2,8,1,14,33,14,18,36)) # this code only grabs 30 from each journal, which is a random number SIMILAR to the 
 #numbers of articles in the open access journals
 # Number of articles to pull should be from NumbArtOA dataframe
 
