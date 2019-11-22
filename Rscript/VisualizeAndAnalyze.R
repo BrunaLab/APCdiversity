d11353171b5839e2902e234a1f0ac752827d2ffc
Jesse Borden <jborden@ufl.edu>
2019-11-21 22:33:50 -0500

    Finally get our first plot

--- Rscript/VisualizeAndAnalyze.R
+++ Rscript/VisualizeAndAnalyze.R
@@ -11,7 +11,7 @@ library(dplyr)
 DivMetricsFullPools <- read.csv("CleanData/DivMetricsFullPools.csv", header = TRUE)
 FirstDivMetricsByJrnl <- read.csv("CleanData/FirstDivMetricsByJrnl.csv", header = TRUE)
 PropSampsSimpsDivsPW <- read.csv("CleanData/ProporSampsSimpsDivPW.csv", header = TRUE)
-
+PropSampsSimpsDivsPW <- gather(PropSampsSimpsDivsPW)
 
 #over all richness for PW and OA each itteration (OA should stay the same)
 FirstRich1000 <- merge(FirstRich1000, FirstAuthDiv, by = "JournalAndType")
@@ -27,6 +27,33 @@ MeanRichOA <- mean(colMeans(FirstRich1000_OA))
 MeanRichPW <- as.data.frame(colMeans(FirstRich1000_PW))
 MeanRichPW$MeanRich <- MeanRichPW$`colMeans(FirstRich1000_PW)`
 MeanOfMeanRichPW <- mean(MeanRichPW$MeanRich)
+
+
+#
+# Plots
+#
+# this is a plot of the over all diversity values calculated 1000 times
+# by subsetting the total PW pool by the same number as are in the OA
+# journals and it is proportionally the same as the OA journals between
+# specific mirror journals
+ggplot(PropSampsSimpsDivsPW, aes(x = value))+
+  geom_histogram()+
+  xlim(6,14)+
+  geom_segment(aes(x = DivMetricsFullPools$OADiversity, y = 0,
+                   xend = DivMetricsFullPools$OADiversity, yend = 170), 
+               color = "red")+
+  ggtitle("Histogram of PW Simpsons Inverse Diversity,
+      taken from 1000 proportional subsamples of the PW pool,
+      red line = OA diversity")
+
+
+
+
+
+
+
+
+
 #HISTOGRAM OF PW JOURNAL AVERAGE OVERALL RICHNESS with OA AVERAGE RICHNESS
 ggplot(MeanRichPW, aes(x = MeanRich))+
   geom_histogram()+
