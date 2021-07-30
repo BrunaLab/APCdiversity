73f265f16d00bdf312d0b74939034bbe9574d3af
Emilio M. Bruna <embruna@ufl.edu>
2021-07-30 02:07:18 -0400

    update for MS

--- Rscript/functions_ms/n_OA_plots.R
+++ Rscript/functions_ms/n_OA_plots.R
@@ -44,7 +44,10 @@ n_OA_plots <- function(AllData, n_countries) {
     slice(1) %>%
     ungroup() %>%
     select(First_Author_Country, Code, IncomeGroup, Region) %>%
-    unique()
+    group_by(Code) %>%
+    slice(1) %>%
+    ungroup() %>%
+    arrange(Code)
 
   plot_data <- left_join(plot_data, Codes)
 
@@ -74,55 +77,6 @@ n_OA_plots <- function(AllData, n_countries) {
   library(RColorBrewer)
   library(ggrepel)
 
-
-  # p1<-ggplot(plot_data, aes(x=nOA,
-  #                           y=nPW,
-  #                           color=IncomeGroup,
-  #                           fill=IncomeGroup,
-  #                           label=Code)) +
-  #   geom_point(size=8,
-  #              color="black",
-  #              shape=21,
-  #              position=position_dodge(width=0.2)) +
-  #   geom_abline(intercept = 0,
-  #               slope = 1,
-  #               color="darkgray",
-  #               size=1.5,
-  #               linetype="dashed") +
-  #   scale_fill_manual(values=c("#F7FBFF","#9ECAE1","#4292C6","#084594"),
-  #                     name="National Income Category",
-  #                     breaks=c("High", "Upper-middle","Lower-middle","Low"))+
-  #   scale_y_continuous(limits = c(0, 350),breaks = seq(0,340, by=20),expand=c(0,0.1))+
-  #   scale_x_continuous(limits = c(0, 250),breaks = seq(0,240, by=20),expand=c(0,0.1))
-  #
-  #
-  # p1<-p1+
-  #   theme_classic()+
-  #
-  #   geom_text_repel(segment.color = "black",
-  #                   color="black",
-  #                   size=8,
-  #                   box.padding = 0.4,
-  #                   max.overlaps = Inf) +
-  #
-  #   labs(x = "No. of articles in Mirror journals",
-  #        y = "No. of OA articles in Parent journals")+
-  #   theme(
-  #     # legend.position = "right",
-  #     axis.text.x = element_text(size=20),
-  #     axis.text.y = element_text(size=20),
-  #     axis.title.x=element_text(colour="black", size = 24, vjust=-0.5),
-  #     axis.title.y=element_text(colour="black", size = 24, vjust=-0.5,
-  #                               margin = margin(t = 0, r = 20, b = 0, l = 0)),
-  #     plot.title = element_text(size=8),
-  #     legend.text = element_text(colour="black", size = 16, vjust=0.5),
-  #     legend.title = element_text(size=20),
-  #     legend.position=c(0.9, 0.2),
-  #     plot.margin =unit(c(1.5,1.5,1.5,1.5), "lines")
-  #     )
-  # p1
-
-
   p1 <- ggplot(plot_data, aes(
     x = perc_oa,
     y = perc_pw,
@@ -131,40 +85,44 @@ n_OA_plots <- function(AllData, n_countries) {
     label = Code
   )) +
     geom_point(
-      size = 4,
+      size = 6,
       color = "black",
       shape = 21,
       position = position_dodge(width = 0.2)
     ) +
-    geom_abline(
-      intercept = 100,
-      slope = -1,
-      color = "darkgray",
-      size = 1.5,
-      linetype = "dashed"
-    ) +
+    # geom_hline(
+    #   yintercept = 50,
+    #   color = "darkgray",
+    #   size = 1.5,
+    #   linetype = "dashed"
+    # ) +
+    # geom_vline(
+    #   xintercept = 50,
+    #   color = "darkgray",
+    #   size = 1.5,
+    #   linetype = "dashed"
+    # ) +
     scale_fill_manual(
       values = c("#F7FBFF", "#9ECAE1", "#4292C6", "#084594"),
       name = "National Income Category",
       breaks = c("High", "Upper-middle", "Lower-middle", "Low")
     ) +
-    scale_y_continuous(limits = c(0, 100), 
-                       breaks = seq(0, 100, by = 10),
+    scale_x_continuous(limits = c(0, 50), 
+                       breaks = seq(0, 50, by = 10),
                        expand = c(0, 0.1)) +
-    scale_x_continuous(limits = c(0, 100),
-                       breaks = seq(0, 100, by = 10),
+    scale_y_continuous(limits = c(50, 100),
+                       breaks = seq(50, 100, by = 10),
                        expand = c(0, 0.1))
 
 
   p1 <- p1 +
     theme_classic() +
-
     geom_text_repel(
       segment.color = "black",
       color = "black",
-      size = 5,
-      box.padding = 0.15,
-      max.overlaps = 20
+      size = 7,
+      box.padding = 0.2,
+      max.overlaps = 25
     ) +
 
     labs(
@@ -183,7 +141,7 @@ n_OA_plots <- function(AllData, n_countries) {
       plot.title = element_text(size = 8),
       legend.text = element_text(colour = "black", size = 16, vjust = 0.5),
       legend.title = element_text(size = 20),
-      legend.position = c(0.85, 0.5),
+      legend.position = c(0.85, 0.65),
       plot.margin = unit(c(1.5, 1.5, 1.5, 1.5), "lines")
     )
   p1
